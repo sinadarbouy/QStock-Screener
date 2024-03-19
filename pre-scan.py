753405046c95f1fe573016863d0e941b655ca30d
sinadarbouy <sinadarbouy@gmail.com>
2024-03-19 23:22:08 +0100

    Update file paths in pre-scan.py

--- pre-scan.py
+++ pre-scan.py
@@ -12,14 +12,14 @@ def calculate_price(ticker):
 def pre_scan():
   
   # making dataframe  
-  df = pd.read_csv("/Users/cna/personal/trade/nasdaq_screener_1710370048684.csv")
+  df = pd.read_csv("data/nasdaq_screener_1710370048684.csv")
   # Filtering based on Country column
   US_df = df[df['Country'] == 'United States']
 
   US_df = US_df[~US_df.iloc[:, 0].str.contains("\^")]
   print("pre scan start:",len(US_df))
   US_df['currentPrice'] =US_df.apply(calculate_price, axis=1)
-  US_df.to_csv('pre_scan.csv', index=False)
+  US_df.to_csv('data/pre_scan.csv', index=False)
   
   print("Pre-scan completed. Total entries after pre-scan:", len(US_df))
 
