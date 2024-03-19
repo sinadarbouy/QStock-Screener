30fae9736797c64d923f281143d40ee0ea6d055b
sinadarbouy <sinadarbouy@gmail.com>
2024-03-19 19:10:40 +0100

    Add .gitignore file and remove tess.py

--- tess.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-dataframe = pd.read_csv("currentPrice.csv")
-print(len(dataframe))
-dataframe = dataframe[dataframe["currentPrice"] < 170]
-print(len(dataframe))
-dataframe = dataframe[dataframe["currentPrice"] != 0]
-print(len(dataframe))
