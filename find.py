971aa01616d4765cee89153d843ad67040f979cc
sinadarbouy <sinadarbouy@gmail.com>
2024-03-23 12:00:52 +0100

    Delete unnecessary files and functions

--- find.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-dataframe.cs
-dataframe['volume_dollars_1day'] = pd.to_numeric(dataframe['volume_dollars_1day'], errors='coerce')
-df = dataframe[dataframe['volume_dollars_1day'] > 20000]
-# Select the top 27% of the data
-top_27_percent_df = sorted_df.head(top_27_percent)
-top_27_percent_df.to_csv('top_27_percent_df.csv', index=False)
-print(top_27_percent_df)
