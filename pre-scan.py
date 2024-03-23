9f28434cf3af3728ab730ba5eeea22ddf2aa8c07
sinadarbouy <sinadarbouy@gmail.com>
2024-03-23 11:57:31 +0100

    Add multiprocessing to pre-scan.py for faster execution

--- pre-scan.py
+++ pre-scan.py
@@ -1,26 +1,46 @@
 import yfinance as yf
 import pandas as pd
+import multiprocessing
+import numpy as np
 
 def calculate_price(ticker):
-  stock = yf.Ticker(ticker["Symbol"])
-  try:
-    currentPrice = stock.info["currentPrice"]
-    return currentPrice
-  except:
-    return 0
+    stock = yf.Ticker(ticker["Symbol"])
+    try:
+        currentPrice = stock.info["currentPrice"]
+        return currentPrice
+    except:
+        return 0
+
+def calculate_prices_for_chunk(chunk):
+    return chunk.apply(calculate_price, axis=1)
 
 def pre_scan():
-  
-  # making dataframe  
-  df = pd.read_csv("data/nasdaq_screener_1710370048684.csv")
-  # Filtering based on Country column
-  US_df = df[df['Country'] == 'United States']
+    # Making dataframe  
+    df = pd.read_csv("data/nasdaq_screener_1710370048684.csv")
+    
+    # Filtering based on Country column
+    US_df = df[df['Country'] == 'United States']
+    
+    # Remove rows with symbols containing "^"
+    US_df = US_df[~US_df.iloc[:, 0].str.contains("\^")]
+    
+    print("Pre scan start:", len(US_df))
+    
+    # Split dataframe into chunks
+    num_cores = multiprocessing.cpu_count()
+    chunks = np.array_split(US_df, num_cores)
+    
+    # Create a multiprocessing Pool
+    with multiprocessing.Pool(num_cores) as pool:
+        results = pool.map(calculate_prices_for_chunk, chunks)
+    
+    # Concatenate results
+    US_df['currentPrice'] = pd.concat(results, ignore_index=True)
+    
+    # Save the result to CSV
+    US_df.to_csv('data/pre_scan.csv', index=False)
+    
+    print("Pre-scan completed. Total entries after pre-scan:", len(US_df))
 
-  US_df = US_df[~US_df.iloc[:, 0].str.contains("\^")]
-  print("pre scan start:",len(US_df))
-  US_df['currentPrice'] =US_df.apply(calculate_price, axis=1)
-  US_df.to_csv('data/pre_scan.csv', index=False)
-  
-  print("Pre-scan completed. Total entries after pre-scan:", len(US_df))
-
-pre_scan()
+if __name__ == '__main__':
+    pre_scan()
