0c75a9dccd8ec1deb29ca4def791ccb65573e46a
sinadarbouy <sinadarbouy@gmail.com>
2024-04-06 18:51:54 +0200

    Refactor pre-scan script for improved performance and readability

--- scripts/pre-scan.py
+++ scripts/pre-scan.py
@@ -4,8 +4,8 @@ import multiprocessing
 import numpy as np
 
 def calculate_price(ticker):
-    stock = yf.Ticker(ticker["Symbol"])
     try:
+        stock = yf.Ticker(ticker["Symbol"])
         currentPrice = stock.info["currentPrice"]
         return currentPrice
     except:
@@ -17,30 +17,24 @@ def calculate_prices_for_chunk(chunk):
 def pre_scan():
     # Making dataframe  
     df = pd.read_csv("data/nasdaq_screener_1710370048684.csv")
-    
-    # Filtering based on Country column
-    US_df = df[df['Country'] == 'United States']
-    
-    # Remove rows with symbols containing "^"
-    US_df = US_df[~US_df.iloc[:, 0].str.contains("\^")]
-    
-    print("Pre scan start:", len(US_df))
+
+    print("Pre scan start:", len(df))
     
     # Split dataframe into chunks
     num_cores = multiprocessing.cpu_count()
-    chunks = np.array_split(US_df, num_cores)
+    chunks = np.array_split(df, num_cores)
     
     # Create a multiprocessing Pool
     with multiprocessing.Pool(num_cores) as pool:
         results = pool.map(calculate_prices_for_chunk, chunks)
     
     # Concatenate results
-    US_df['currentPrice'] = pd.concat(results, ignore_index=True)
+    df['currentPrice'] = pd.concat(results, ignore_index=True)
     
     # Save the result to CSV
-    US_df.to_csv('data/pre_scan.csv', index=False)
+    df.to_csv('data/pre_scan.csv', index=False)
     
-    print("Pre-scan completed. Total entries after pre-scan:", len(US_df))
+    print("Pre-scan completed. Total entries after pre-scan:", len(df))
 
 if __name__ == '__main__':
     pre_scan()
